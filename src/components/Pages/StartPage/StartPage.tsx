e0ebce2330dfcd2ed143b9ea142c5ad3ff27c5a5
Nikita Filin <nf@hookprod.com>
2025-09-18 09:27:22 +0300

    fix: правки

--- src/components/Pages/StartPage/StartPage.tsx
+++ src/components/Pages/StartPage/StartPage.tsx
@@ -6,14 +6,13 @@ export const StartPage = () => {
     <PageContainer>
       <Wrapper>
         <Title>Дорогие друзья!</Title>
-        <Subtitle>
-          Мы с огромным удовольствием приглашаем вас на праздник в честь
+        <Subtitle>{`В жизни так сложно найти свою половинку ${":)"} у нас получилось!!!`}</Subtitle>
+        <Subtitle style={{ marginTop: "8px" }}>
+          Приглашаем вас на праздничный ужин, разделить с нами это радостное
+          событие с{" "}
+          <span style={{ fontSize: "40px", color: "#36827F" }}>20</span>-летним
+          стажем!
         </Subtitle>
-        <Subtitle>
-          <span style={{ fontSize: "40px", color: "#36827F" }}>20</span>
-          -летия
-        </Subtitle>
-        <Subtitle>нашей свадьбы</Subtitle>
       </Wrapper>
     </PageContainer>
   );
